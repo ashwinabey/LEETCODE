3f32cbb180e4766a194ba3db1acddbbefee9fa43
ashwinabey <153995008+ashwinabey@users.noreply.github.com>
2026-02-21 09:22:26 +0530

    Time: 5 ms (59.05%) | Memory: 101.2 MB (69.79%) - LeetSync

--- 2236-maximum-twin-sum-of-a-linked-list/maximum-twin-sum-of-a-linked-list.java
+++ 2236-maximum-twin-sum-of-a-linked-list/maximum-twin-sum-of-a-linked-list.java
@@ -1,36 +1,44 @@
-/**
- * Definition for singly-linked list.
- * public class ListNode {
- *     int val;
- *     ListNode next;
- *     ListNode() {}
- *     ListNode(int val) { this.val = val; }
- *     ListNode(int val, ListNode next) { this.val = val; this.next = next; }
- * }
- */
 class Solution {
+
     public int pairSum(ListNode head) {
+        ListNode slow = head;
+        ListNode fast = head;
+
         
-        List<Integer> list = new ArrayList<>();
-
-
-        while(head!=null){
-            list.add(head.val);
-            head=head.next;
+        while (fast != null && fast.next != null) {
+            slow = slow.next;
+            fast = fast.next.next;  
         }
 
-        int i = 0;
-        int j = list.size()-1;
+        
+        ListNode p2 = reverseLL(slow);
+        ListNode p1 = head;
 
-        int max=Integer.MIN_VALUE;
+        int max = 0;
 
-        while(i<j){
-            int candidate = list.get(i)+list.get(j);
-            max=Math.max(max,candidate);
+        
+        while (p2 != null) {
+            int candidate = p1.val + p2.val;
+            max = Math.max(max, candidate);
 
-            i++;
-            j--;
+            p1 = p1.next;
+            p2 = p2.next;
         }
+
         return max;
+    }
+
+    public ListNode reverseLL(ListNode head) {
+        ListNode prev = null;
+        ListNode curr = head;
+
+        while (curr != null) {
+            ListNode nextNode = curr.next;
+            curr.next = prev;
+            prev = curr;
+            curr = nextNode;
+        }
+
+        return prev;
     }
 }
