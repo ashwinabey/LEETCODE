88f576eb0762e9cf1b36504e292d89eb0b85d7da
ashwinabey <153995008+ashwinabey@users.noreply.github.com>
2026-02-24 19:44:30 +0530

    Time: 35 ms (82.88%) | Memory: 51.5 MB (73.63%) - LeetSync

--- 2054-the-number-of-the-smallest-unoccupied-chair/the-number-of-the-smallest-unoccupied-chair.java
+++ 2054-the-number-of-the-smallest-unoccupied-chair/the-number-of-the-smallest-unoccupied-chair.java
@@ -1,21 +1,31 @@
 class Solution {
     public int smallestChair(int[][] times, int targetFriend) {
-        int targetfriendarrival= times[targetFriend][0];
+        
+        int target=times[targetFriend][0];
+        Arrays.sort(times,(a,b) -> a[0]-b[0]);
 
-        Arrays.sort(times,(a,b)-> a[0]-b[0]);// sorting by arrival 
+        PriorityQueue<Integer> free = new PriorityQueue<>();
+        PriorityQueue<int[]> busy = new PriorityQueue<>((a,b)-> a[0]-b[0]);
 
-        int [] chairs = new int[times.length];// end time 
+        int chairno = 0 ;
 
-        for(int i = 0 ; i<times.length; i++){
-            for(int j = 0 ; j<chairs.length;j++){
-                if(times[i][0]>=chairs[j]){
-                    if(targetfriendarrival==times[i][0]){
-                        return j;
-                    }
-                    chairs[j]=times[i][1];
-                    break;
-                }
+        for (int []t : times){
+            int arrival = t[0];
+            int leave =t[1];
+
+            while(!busy.isEmpty() && busy.peek()[0]<=arrival){
+                free.add(busy.poll()[1]);
             }
+            int chair;
+            if(!free.isEmpty()){
+                chair=free.poll();
+            }else{
+                chair=chairno++;
+            }
+            if(arrival==target){
+                return chair;
+            }
+            busy.add(new int[]{leave,chair});
         }
         return -1;
     }
